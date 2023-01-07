22e6c479c5ee84db0fd50375041b932708d1d2d6
황윤정 <107980438+jjenniyun@users.noreply.github.com>
2023-01-07 14:54:41 +0000

    python_web_scrapping(23.01.07)

---
+++ extractors/wwr.py
@@ -0,0 +1,30 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+def extract_wwr_jobs(keyword):
+  url = "https://weworkremotely.com/remote-jobs/search?term="
+  response = get(f"{url}{keyword}")
+  if response.status_code != 200:
+    print("Cant request website")
+  else:
+    results = []
+    soup = BeautifulSoup(response.text, "html.parser")
+    jobs = soup.find_all('section',class_="jobs")
+    for job_section in jobs:
+      job_posts = job_section.find_all('li')
+      job_posts.pop(-1) #view_all 제거
+      for post in job_posts:
+        anchors = post.find_all('a')
+        anchor = anchors[1]
+        link = anchor['href'] # 딕셔너리
+        # 리스트에 넣어서 list 순서대로 값을 저장(시간 절약)
+        company, kind, region = anchor.find_all('span', class_="company") # 회사이름, 직무, 일하는 지역
+        title = anchor.find('span', class_='title') # 하나의 항목 찾기
+        job_data = {
+          'link': f"https://weworkremotely.com/{link}",
+          'company':company.string,
+          'region':region.string,
+          'position':title.string
+        }
+        results.append(job_data)
+    return results
--- main.py
+++ main.py
@@ -1,27 +1,6 @@
 from requests import get
-# get: 하나의 url를 가져오게 함
-# 200코드
-websites = [
-  "google.com", 
-  "airbnb.com", 
-  "https://twitter.com", 
-  "facebook.com",
-  "https://tiktok.com"
-]
+from bs4 import BeautifulSoup
+from extractors.wwr import extract_wwr_jobs
 
-results = {}
-
-# url formatting
-for website in websites:
-  if not website.startswith("https://"):
-    website = f"https://{website}"
-  response = get(website)
-  # print(response.status_code) # response[200]: 성공적으로 응답함
-  if response.status_code == 200:
-    results[website] = "OK"
-    # print(f"{website} is OK")
-  else:
-    results[website] = "FAILED"
-    #print(f"{website} not OK")
-    
-print(results)
+#jobs= extract_wwr_jobs("python")
+#print(jobs)
